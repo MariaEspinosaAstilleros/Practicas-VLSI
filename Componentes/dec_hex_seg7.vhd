3d558416e161b8a4af88b9a0f0fc8b7706c204bf
MariaEspinosaAstilleros <43780193+MariaEspinosaAstilleros@users.noreply.github.com>
2020-04-16 19:58:03 +0200

    Código comentado

--- Componentes/dec_hex_seg7.vhd
+++ Componentes/dec_hex_seg7.vhd
@@ -4,10 +4,10 @@ use ieee.std_logic_unsigned.all;
 
 ENTITY dec_hex_seg7 IS 
     PORT (
-        hex: IN STD_LOGIC_VECTOR(5 to 8);
-        enable : IN STD_LOGIC;
-        DP : OUT STD_LOGIC;
-        display : OUT STD_LOGIC_VECTOR(1 to 7)
+        hex: IN STD_LOGIC_VECTOR(5 to 8);       -- codigo hexadecimal a decodificar
+        enable : IN STD_LOGIC;                  -- señal de habilitacion
+        DP : OUT STD_LOGIC;                     -- led decimal point
+        display : OUT STD_LOGIC_VECTOR(1 to 7)  -- display de 7 segmentos
          );
 END dec_hex_seg7;
 
@@ -15,8 +15,8 @@ ARCHITECTURE DecFunc OF dec_hex_seg7 IS
 BEGIN 
     PROCESS(hex)
     BEGIN
-        IF enable = '1' THEN 
-            CASE hex is
+        IF enable = '1' THEN -- Si la señal de habilitación está activo 
+            CASE hex is -- Asignacion del display dependiendo del código que se vaya a decodificar
                 WHEN "0000" => display <= "0000000"; -- 0
                 WHEN "0001" => display <= "0110000"; -- 1
                 WHEN "0010" => display <= "1101101"; -- 2
@@ -35,11 +35,11 @@ BEGIN
                 WHEN OTHERS => display <= "1000111"; -- F
             END CASE;
         ELSE 
-            display <= "0000000";
+            display <= "0000000"; -- Si la señal de habilitación está apagada el display valdrá 0
         END IF;
     END PROCESS;
 
-    DP <= '1' WHEN (hex >= 9) ELSE '0';
+    DP <= '1' WHEN (hex >= 9) ELSE '0';  -- El DP se activará cuando su valor sea 9 o mayor
 
 
 END DecFunc;
--- Proyectos/lab6/DecodificadorDisplay/lab6.vhd
+++ Proyectos/lab6/DecodificadorDisplay/lab6.vhd
@@ -4,22 +4,22 @@ USE ieee.std_logic_unsigned.all;
 
 ENTITY lab6 IS 
     PORT (
-        DIP_SW : IN STD_LOGIC_VECTOR(5 to 8);
-        KEY_EX : IN STD_LOGIC;
-        DP : OUT STD_LOGIC;
-        DISP: OUT STD_LOGIC_VECTOR (1 to 7)
+        DIP_SW : IN STD_LOGIC_VECTOR(5 to 8);  -- dip switches externos
+        KEY_EX : IN STD_LOGIC;                 -- pulsador externo
+        DP : OUT STD_LOGIC;                    -- led DP
+        DISP: OUT STD_LOGIC_VECTOR (1 to 7)    -- display de 7 segmentos
     );
 END lab6;
 
 ARCHITECTURE funcLab6 OF lab6 IS 
-COMPONENT dec_hex_seg7 
+COMPONENT dec_hex_seg7  -- componente del display de 7 segmentos
     PORT (
         hex: IN STD_LOGIC_VECTOR(5 to 8);
         enable : IN STD_LOGIC;
         DP : OUT STD_LOGIC;
         display : OUT STD_LOGIC_VECTOR(1 to 7));
 END COMPONENT;
-BEGIN 
+BEGIN  --Asignamos las entradas y salidas a los atributos del componente
     dec : dec_hex_seg7 port map (hex => DIP_SW(5 to 8), enable => KEY_EX, 
         DP => DP,  display => DISP(1 to 7));
 END funcLab6;
